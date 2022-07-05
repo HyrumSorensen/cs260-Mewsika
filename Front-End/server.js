9c36d0d3f0fb0111556fc29105076cdc9fe4961c
Hyrum Sorensen <hyrumgary@gmail.com>
2022-07-05 15:26:06 -0500

    procfile

--- Front-End/server.js
+++ Front-End/server.js
@@ -8,13 +8,13 @@ const port = process.env.PORT || 4005
 app.use(express.json)
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './home.html'))
+    res.sendFile(path.join(__dirname, '/home.html'))
 })
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, './home.css'))
+    res.sendFile(path.join(__dirname, '/home.css'))
 })
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './home.js'))
+    res.sendFile(path.join(__dirname, '/home.js'))
 })
 
 
