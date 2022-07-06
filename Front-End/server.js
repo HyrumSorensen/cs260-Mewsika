3900c7af5d69d12c5ad4ca18fc0bbf28ace752a1
Hyrum Sorensen <hyrumgary@gmail.com>
2022-07-06 10:32:21 -0500

    day 1

--- Front-End/server.js
+++ Front-End/server.js
@@ -3,12 +3,13 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 4005
+const port = process.env.PORT || 6900
 
-app.use(express.json)
+app.use(express())
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/home.html'))
+    res.sendFile(path.join(__dirname, './home.html'))
 })
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '/home.css'))
@@ -16,6 +17,19 @@ app.get('/css', (req, res) => {
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '/home.js'))
 })
+app.get('/keyboard', (req, res) => {
+    res.sendFile(path.join(__dirname, '/keyboard.html'))
+})
+app.get('/mySongs', (req, res) => {
+    res.sendFile(path.join(__dirname, '/mySongs.html'))
+})
+app.get('/generator', (req, res) => {
+    res.sendFile(path.join(__dirname, '/generator.html'))
+})
+app.get('/aboutme', (req, res) => {
+    res.sendFile(path.join(__dirname, '/aboutme.html'))
+})
+
 
 
 app.listen(port, () => {
